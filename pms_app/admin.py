2b2bcb0e15d5e9b80f5329fddb63de797d4e692d
delna <sajongd@gmail.com>
2022-12-17 15:01:58 +0530

    model

--- pms_app/admin.py
+++ pms_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import*
+admin.site.register(patient)
+admin.site.register(Doctor)
+admin.site.register(Nurse)
+admin.site.register(PatientFeedback)
 # Register your models here.
--- pms_app/models.py
+++ pms_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class patient(models.Model):
+    name=models.CharField(max_length=50,default="")
+    phone_no=models.IntegerField(max_length=10,default="")
+    address=models.CharField(max_length=200,default="")
+    def __str__(self):
+         return self.name
+class Doctor(models.Model):
+    name=models.CharField(max_length=50,default="")
+    department=models.CharField(max_length=100,default="")
+    category=models.CharField(max_length=50,default="")
+    def __str__(self):
+         return self.name
+class Nurse(models.Model):
+    name=models.CharField(max_length=50,default="")
+    category=models.CharField(max_length=50,default="")
+    def __str__(self):
+        return self.name
+
+class Prescription(models.Model):
+    prescription=models.CharField(max_length=500,default="")
+    doctor_id=models.ForeignKey(Doctor, related_name = "doctor",on_delete=models.PROTECT)
+
+class PatientFeedback(models.Model):
+    patient_id=models.ForeignKey(patient, related_name = "patient",on_delete=models.PROTECT)
+    feedback=models.CharField(max_length=500,default="")
+def __str__(self):
+        return self.feedback
+class discussion(models.Model):
+    discussion=models.CharField(max_length=500,default="")
+def __str__(self):
+        return self.discussion
